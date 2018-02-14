832963c912500463daf276af45b18104a3b7ae7f
abdullahsumbal <abdullahsumbal@gmail.com>
2018-02-13 21:11:49 -0500

    Bug fixes. works with the local api

--- app/admin/js/login.js
+++ app/admin/js/login.js
@@ -6,22 +6,22 @@ function validate(){
 	var password = document.forms["loginForm"]["password"].value;
 
 	var xhr = new XMLHttpRequest();
-	//alert ('http://127.0.0.1:5000/auth?username='+username+"&pwd="+password);
-	xhr.open('GET', 'http://127.0.0.1:5000/auth?username='+username+"&pwd="+password, true);
-	xhr.send();
-	xhr.addEventListener("readystatechange", processRequest, false);
-	function processRequest(e) {
-	    if (xhr.readyState == 4 && xhr.status == 200) {
-	        var response = xhr.responseText;
-	        if(response == "yes"){
-	        	alert("log in successfully"); 
-	        	return true;
-	        }
-	        alert("Log in unsuccessful");
-	    }
-	    else if (xhr.readyState == 0){
-	    	alert("Something went wrong. Try agian");
-	    }
+	//console.log('http://127.0.0.1:5000/auth?username='+username+"&pwd="+password);
+ 	xhr.open('GET', 'http://127.0.0.1:5000/auth?username='+username+"&pwd="+password, true);
+ 	xhr.send();
+ 	xhr.addEventListener("readystatechange", processRequest, false);
+ 	function processRequest(e) {
+ 	    if (xhr.readyState == 4 && xhr.status == 200) {
+	        var response = JSON.parse(xhr.responseText);
+	        if(response){
+	        	alert("log in successfully");
+	        	// Change path to correct page 
+	        	window.location.href = "http://google.com";
+ 	        }
+	        else{
+	        	alert("Log in unsuccessful");
+			}
+ 		}
 	}
 	return false;
 }
