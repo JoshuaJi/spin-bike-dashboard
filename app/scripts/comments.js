d76f07e103a4d76f729430ef6c1a1d7b273a3a8f
Lee Dennis <leealandennis@gmail.com>
2018-03-28 13:21:01 -0400

    evetyrhing shall workgit statusgit status

--- app/scripts/comments.js
+++ app/scripts/comments.js
@@ -19,18 +19,22 @@ comments = {
 
   loadPhotos: function () {
     $.get('https://spin-bike-api.herokuapp.com/get_all_images', function (data) {
+      data = data.substring(2, data.length - 1);
       console.log(data);
+      $('#cardContent').prepend('<img src="data:image/jpeg;base64,' + data + '"/>');
     });
   },
 
   submitPhoto: function () {
     var url = $("#image").attr('src');
-    var raw_base64 = url.split("base64,")[1]
+    var rawBase64 = url.split("base64,")[1];
+    console.log(rawBase64);
     var formDataForPut = new FormData();
 
-    var image = "{\"base\":\""+ raw_base64+"\"}";
-    formDataForPut.append("data",image);
-    console.log(settings)
+    var image = {
+      'base': rawBase64
+    };
+    formDataForPut.append("data", JSON.stringify(image));
 
 
     var settings = {
@@ -46,9 +50,10 @@ comments = {
       "mimeType": "multipart/form-data",
       "data": formDataForPut
     };
-    console.log(settings)
+
     $.ajax(settings).done(function (response) {
       console.log(response);
+      location.reload();
     });
 
   }
